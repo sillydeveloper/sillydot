d52fd0176b16a3816a28c4ed9b8b7945dbb491dd
Andrew Ettinger <sillydeveloper@gmail.com>
2015-01-30 09:20:04 -0800

    Lowercase variables

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
-user= `whoami`.gsub(/\n/,'') # whoami appends a new line, clean it out
-user_directory = "/Users/#{USER}"
+user = `whoami`.gsub(/\n/,'') # whoami appends a new line, clean it out
+user_directory = "/Users/#{user}"
 project_directory = "#{user_directory}/Projects/sillydot"
 
 Dir.entries("#{project_directory}/dotfiles").each do |dotfile|
@@ -9,7 +9,7 @@ Dir.entries("#{project_directory}/dotfiles").each do |dotfile|
     `mv #{user_directory}/#{dotfile} #{user_directory}/#{dotfile}.orig` if File.exist?("#{user_directory}/#{dotfile}")
   
     p "Linking #{dotfile}..."
-    `ln -s #{project_directory}/#{dotfile} ~/#{dotfile}`
+    `ln -s #{project_directory}/dotfiles/#{dotfile} ~/#{dotfile}`
   end
 end
 
